6fa3fbadea15799f5fb5cb1fcc5d42fcf4b00798
wal72thela <walt2thela@gmail.com>
2023-08-15 20:27:59 +0200

    HTML

--- auth/forms.py
+++ auth/forms.py
@@ -15,6 +15,14 @@ from wtforms import ValidationError,validators
 from models import User
 
 
+class login_form(FlaskForm):
+    email = StringField(validators=[InputRequired(), Email(), Length(1, 64)])
+    password = PasswordField(validators=[InputRequired(), Length(min=8, max=72)])
+    # Placeholder labels to enable form rendering
+    username = StringField(
+        validators=[Optional()]
+    )
+
 
 class register_form(FlaskForm):
     username = StringField(
@@ -29,11 +37,20 @@ class register_form(FlaskForm):
         ]
     )
     email = StringField(validators=[InputRequired(), Email(), Length(1, 64)])
-    password = PasswordField(validators=[InputRequired(), Length(8, 72)])
-    confirm_password = PasswordField(
+    pwd = PasswordField(validators=[InputRequired(), Length(8, 72)])
+    cpwd = PasswordField(
         validators=[
             InputRequired(),
             Length(8, 72),
-            EqualTo("password", message="Passwords must match !"),
+            EqualTo("pwd", message="Passwords must match !"),
         ]
     )
+
+
+    def validate_email(self, email):
+        if User.query.filter_by(email=email.data).first():
+            raise ValidationError("Email already registered!")
+
+    def validate_username(self, username):
+        if User.query.filter_by(username=username.data).first():
+            raise ValidationError("Username already taken!")
