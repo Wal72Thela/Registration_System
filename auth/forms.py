63d227ae102fad7a95bc1970aad85a17aaf01ba5
wal72thela <walt2thela@gmail.com>
2023-08-15 20:21:35 +0200

    validating

---
+++ auth/forms.py
@@ -0,0 +1,39 @@
+from wtforms import (
+    StringField,
+    PasswordField,
+    BooleanField,
+    IntegerField,
+    DateField,
+    TextAreaField,
+)
+
+from flask_wtf import FlaskForm
+from wtforms.validators import InputRequired, Length, EqualTo, Email, Regexp ,Optional
+import email_validator
+from flask_login import current_user
+from wtforms import ValidationError,validators
+from models import User
+
+
+
+class register_form(FlaskForm):
+    username = StringField(
+        validators=[
+            InputRequired(),
+            Length(3, 20, message="Please provide a valid name"),
+            Regexp(
+                "^[A-Za-z][A-Za-z0-9_.]*$",
+                0,
+                "Usernames must have only letters, " "numbers, dots or underscores",
+            ),
+        ]
+    )
+    email = StringField(validators=[InputRequired(), Email(), Length(1, 64)])
+    password = PasswordField(validators=[InputRequired(), Length(8, 72)])
+    confirm_password = PasswordField(
+        validators=[
+            InputRequired(),
+            Length(8, 72),
+            EqualTo("password", message="Passwords must match !"),
+        ]
+    )
